82bf829b121e0e76810c68a008019ab74a4e41e4
Marcellus Saputra <marcellussaputra@gmail.com>
2023-02-23 17:43:37 +0100

    Removed transaction list height limit

--- lib/transactionlist.dart
+++ lib/transactionlist.dart
@@ -10,19 +10,16 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: txList.map((tx) {
-            return TransactionCard(
-              id: tx.id,
-              title: tx.title,
-              amount: tx.amount,
-              date: tx.date,
-            );
-          }).toList(),
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: txList.map((tx) {
+          return TransactionCard(
+            id: tx.id,
+            title: tx.title,
+            amount: tx.amount,
+            date: tx.date,
+          );
+        }).toList(),
       ),
     );
   }
