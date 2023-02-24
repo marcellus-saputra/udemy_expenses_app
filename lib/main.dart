9b168ce6327f8d8056c76007424b69eeb5ea98aa
Marcellus Saputra <marcellussaputra@gmail.com>
2023-02-24 16:19:19 +0100

    Refactored main_transaction away, added modal menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_expenses_app/inputfield.dart';
 
-import 'main_transaction.dart';
+import 'transactionlist.dart';
+import 'transaction.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Groceries',
+      amount: 21.12,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void addTransaction(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      transactions.add(newTx);
+    });
+  }
+
+  void startAddTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: InputField(addTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +62,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
+            Builder(
+              builder: (context) => IconButton(
+                onPressed: () => startAddTransaction(context),
+                icon: Icon(Icons.add),
+              ),
             )
           ],
           title: Text('Expenses'),
         ),
-        body: MainTransaction(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                elevation: 5,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: InputField(addTransaction),
+                ),
+              ),
+              Card(
+                child: TransactionList(transactions),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton(
+            onPressed: () => startAddTransaction(context),
+            child: Icon(Icons.add),
+          ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
